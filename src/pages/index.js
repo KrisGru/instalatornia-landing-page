c4cf9280e43c6861c7b2449cd4060bc45db3aef4
KrisGru <k.gruszczynski1002@gmail.com>
2023-11-04 21:14:07 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as Home } from "./home";
+export { default as Home } from "./Home";
 export { default as About } from "./about/NewAbout";
 export { default as Services } from "./services";
 export { default as Contact } from "./Contact";

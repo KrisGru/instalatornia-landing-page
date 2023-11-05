5092e32f24971335add7e19940f485793b232bd1
KrisGru <k.gruszczynski1002@gmail.com>
2023-11-05 09:47:30 +0100

    change syntax for react-18

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faFontAwesome, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
 library.add(fas, faTwitter, faFontAwesome);
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
